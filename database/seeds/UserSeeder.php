e8c6d721468a945bc649036feab8bacb17848e99
= <=>
2020-07-17 06:57:07 -0400

    fixed basic info bugs

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -13,14 +13,23 @@ class UserSeeder extends Seeder
     {
         //
         DB::table('users')->insert([
-            'name' => 'admin',
+            ['name' => 'admin',
             'cnic' => '17101-1234324-1',
             'contact_no' => '+92-51-90800206',
-            'name' => 'admin',
             'email' => 'nbeac@hec.gov.pk',
             'password' => Hash::make('admin!@#$'),
             'user_type' => 'admin',
             'status' => 'active',
+            ],
+            ['name' => 'Sania Tufail',
+            'cnic' => '17101-1234324-1',
+            'contact_no' => '+92-51-90800206',
+            'email' => 'stufail@hec.gov.pk',
+            'password' => Hash::make('admin!@#$'),
+            'user_type' => 'admin',
+            'status' => 'active',
+            ],
+
         ]);
     }
 }
