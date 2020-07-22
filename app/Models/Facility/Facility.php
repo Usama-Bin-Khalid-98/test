26b804f93c2ea619b1b9b7830a5cd934091acc5c
= <=>
2020-07-22 08:26:39 -0400

    worked on facility, fincance migrations

---
+++ app/Models/Facility/Facility.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Facility;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Facility extends Model
+{
+    //
+}
---
+++ database/factories/FinancialInfoFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Facility\FinancialInfo;
+use Faker\Generator as Faker;
+
+$factory->define(FinancialInfo::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
