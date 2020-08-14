e35cc011f90ea725093abc663835ef2d046a3d38
= <=>
2020-08-14 09:57:06 -0400

    added registration list on dashboard, fixed contact InfoController issue, added some more fields in user table for reg, workd on submit registration request etc

--- resources/views/strategic_management/basic_info.blade.php
+++ resources/views/strategic_management/basic_info.blade.php
@@ -285,6 +285,7 @@
                     if(response.success){
                         Notiflix.Notify.Success(response.success);
                     }
+                    window.reload();
                     console.log('response here', response);
                 },
                 error:function(response, exception){
