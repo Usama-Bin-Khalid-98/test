90c2956ca12330f66a121ee9809b4435074aa67b
yoursafi509 <yoursafi509@gmail.com>
2020-08-17 14:44:00 +0500

    New Module Admission & Examination added in side bar from SAR doc. crud & migrations done for three views i-e Credit transfer , Student Transfer & Documentary Evidence.

---
+++ app/Http/Controllers/StudentTransferController.php
@@ -0,0 +1,190 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\StudentTransfer;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Mockery\Exception;
+use Illuminate\Support\Facades\Storage;
+use App\Http\Controllers\Controller;
+use Auth;
+
+class StudentTransferController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware(['auth','verified']);
+        $this->middleware('auth');
+    }
+    public function index()
+    {
+        $campus_id = Auth::user()->campus_id;
+        $department_id = Auth::user()->department_id;
+        
+        $reports = StudentTransfer::with('campus')->where(['campus_id'=> $campus_id,'department_id'=> $department_id])->get();
+        
+        return view('admission_examination.student_transfer', compact('reports'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+         $validation = Validator::make($request->all(), $this->rules(), $this->messages());
+        if($validation->fails())
+        {
+            return response()->json($validation->messages()->all(), 422);
+        }
+        try {
+//            dd($fileName);
+                $path = ''; $imageName = '';
+                if($request->file('file')) {
+                    $imageName ="-file-" . time() . '.' . $request->file->getClientOriginalExtension();
+                    $path = 'uploads/student_transfer';
+                    $diskName = env('DISK');
+                    $disk = Storage::disk($diskName);
+                    $request->file('file')->move($path, $imageName);
+
+                    //dd($request->all());
+                    // $data = $request->replace(array_merge($request->all(), ['cv' => $path.'/'.$imageName]));
+
+                    StudentTransfer::create([
+                        'campus_id' => Auth::user()->campus_id,
+                        'department_id' => Auth::user()->department_id,
+                        'file' => $path.'/'.$imageName, 
+                        'created_by' => Auth::user()->id 
+                ]);
+
+                    return response()->json(['success' => 'Student Transfer added successfully.']);
+                }
+
+        }catch (Exception $e)
+        {
+            return response()->json($e->getMessage(), 422);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\StudentTransfer  $studentTransfer
+     * @return \Illuminate\Http\Response
+     */
+    public function show(StudentTransfer $studentTransfer)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\StudentTransfer  $studentTransfer
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(StudentTransfer $studentTransfer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\StudentTransfer  $studentTransfer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, StudentTransfer $studentTransfer)
+    {
+        $validation = Validator::make($request->all(), $this->update_rules(), $this->messages());
+        if($validation->fails())
+        {
+            return response()->json($validation->messages()->all(), 422);
+        }
+
+        try {
+            $path = ''; $imageName = '';
+            if($request->file('file')) {
+                $imageName ="-file-" . time() . '.' . $request->file->getClientOriginalExtension();
+                $path = 'uploads/student_transfer';
+                $diskName = env('DISK');
+                Storage::disk($diskName);
+                $request->file('file')->move($path, $imageName);
+                // $data = $request->replace(array_merge($request->all(), ['cv' => $path.'/'.$imageName]));
+                StudentTransfer::where('id', $studentTransfer->id)->update(
+                    [
+                    'file' => $path.'/'.$imageName,
+                    'isComplete' => $request->isComplete,
+                    'status' => $request->status,
+                    'updated_by' => Auth::user()->id 
+                    ]
+                );
+
+                return response()->json(['success' => 'Student Transfer updated successfully.']);
+            }
+           StudentTransfer::where('id', $studentTransfer->id)->update([
+               'isComplete' => $request->isComplete,
+               'status' => $request->status,
+               'updated_by' => Auth::user()->id 
+           ]);
+            return response()->json(['success' => 'Student Transfer updated successfully.']);
+
+        }catch (Exception $e)
+        {
+            return response()->json($e->getMessage(), 422);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\StudentTransfer  $studentTransfer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(StudentTransfer $studentTransfer)
+    {
+         try {
+             StudentTransfer::where('id', $studentTransfer->id)->update([
+               'deleted_by' => Auth::user()->id 
+           ]);
+             StudentTransfer::destroy($studentTransfer->id);
+                return response()->json(['success' => 'Record deleted successfully.']);
+         }catch (Exception $e)
+             {
+                return response()->json(['error' => 'Failed to delete record.']);
+             }
+    }
+
+    protected function rules() {
+        return [
+            'file.*' => 'required|file|mimetypes:application/msword,application/pdf|max:2048',
+        ];
+    }
+
+    protected function update_rules() {
+        return [
+            'file.*' => 'file|mimetypes:application/msword,application/pdf|max:2048',
+        ];
+    }
+
+    protected function messages() {
+        return [
+            'required' => 'The :attribute can not be blank.',
+            'file.mimes' => 'Document must be of the following file type: pdf, doc or docx.'
+        ];
+    }
+}
---
+++ database/seeds/CreditTransferSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CreditTransferSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
