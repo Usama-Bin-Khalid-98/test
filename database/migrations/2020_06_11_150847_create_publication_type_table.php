e3b7a47803a09f7bad973c56fb17518d672eb564
= <=>
2020-08-19 08:20:43 -0400

    fixed migrations/seeder issue

--- database/migrations/2020_06_11_150847_create_publication_type_table.php
+++ database/migrations/2020_06_11_150847_create_publication_type_table.php
@@ -16,9 +16,9 @@ class CreatePublicationTypeTable extends Migration
         Schema::create('publication_types', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name', 255);
-            $table->bigInteger('publication_category_id');
+            $table->bigInteger('publication_category_id')->unsigned();
             $table->foreign('publication_category_id')
-                ->references('publication_category_id')
+                ->references('id')
                 ->on('publication_categories');
             $table->enum('status', ['active','inactive'])->default('active');
             $table->softDeletes();
