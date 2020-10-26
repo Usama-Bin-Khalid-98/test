459f7c0c3dc7de2e865d3792ba1ecb8dcff5660f
= <=>
2020-10-26 15:19:00 -0400

    worked on 3.1 tabular format with year

--- resources/views/strategic_management/budgetary_info.blade.php
+++ resources/views/strategic_management/budgetary_info.blade.php
@@ -229,26 +229,26 @@
                                         <option value="2018">2018</option>
                                         <option value="2019">2019</option>
                                         <option value="2020">2020</option>
-                                        <option value="2020">2021</option>
-                                        <option value="2020">2022</option>
-                                        <option value="2020">2023</option>
-                                        <option value="2020">2024</option>
-                                        <option value="2020">2025</option>
-                                        <option value="2020">2026</option>
-                                        <option value="2020">2027</option>
-                                        <option value="2020">2028</option>
-                                        <option value="2020">2029</option>
-                                        <option value="2020">2030</option>
-                                        <option value="2020">2031</option>
-                                        <option value="2020">2032</option>
-                                        <option value="2020">2033</option>
-                                        <option value="2020">2034</option>
-                                        <option value="2020">2035</option>
-                                        <option value="2020">2036</option>
-                                        <option value="2020">2037</option>
-                                        <option value="2020">2038</option>
-                                        <option value="2020">2039</option>
-                                        <option value="2020">2040</option>
+                                        <option value="2021">2021</option>
+                                        <option value="2022">2022</option>
+                                        <option value="2023">2023</option>
+                                        <option value="2024">2024</option>
+                                        <option value="2025">2025</option>
+                                        <option value="2026">2026</option>
+                                        <option value="2027">2027</option>
+                                        <option value="2028">2028</option>
+                                        <option value="2029">2029</option>
+                                        <option value="2030">2030</option>
+                                        <option value="2031">2031</option>
+                                        <option value="2032">2032</option>
+                                        <option value="2033">2033</option>
+                                        <option value="2034">2034</option>
+                                        <option value="2035">2035</option>
+                                        <option value="2036">2036</option>
+                                        <option value="2037">2037</option>
+                                        <option value="2038">2038</option>
+                                        <option value="2039">2039</option>
+                                        <option value="2040">2040</option>
                                     </select>
                                 </div>
                                 <input type="hidden" id="edit_id">
