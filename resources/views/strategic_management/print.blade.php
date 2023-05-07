dfa12317ca3ad5797cf9a8a3fb1d3790c6f7e6c0
Usama Bin Khalid <usama.khalid@arbisoft.com>
2023-05-07 11:02:51 +0500

    Fix: invoice print template

--- resources/views/strategic_management/print.blade.php
+++ resources/views/strategic_management/print.blade.php
@@ -8,13 +8,28 @@
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.7 -->
-    <link rel="stylesheet" href="{{ asset('bower_components/bootstrap/dist/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('bower_components/bootstrap/dist/css/bootstrap.min.css')}}" >
     <!-- Font Awesome -->
     <link rel="stylesheet" href="{{ asset('bower_components/font-awesome/css/font-awesome.min.css')}}">
     <!-- Ionicons -->
     <link rel="stylesheet" href="{{ asset('bower_components/Ionicons/css/ionicons.min.css')}}">
     <!-- Theme style -->
     <link rel="stylesheet" href="{{ asset('dist/css/AdminLTE.min.css')}}">
+    <style type="text/css" media="print">
+      /* @page {
+        size: auto;
+        margin: 10px;
+        
+        
+      } */
+      @media print {
+        body {
+            print-color-adjust: exact;  
+            -webkit-print-color-adjust: exact; 
+        }
+        
+        }
+    </style>
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -26,7 +41,7 @@
     <!-- Google Font -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 </head>
-<body onload="window.print();">
+<body onload="window.print();window.close()">
 <div class="wrapper">
     <!-- Main content -->
     <!-- Main content -->
@@ -42,27 +57,28 @@
         <div class="row">
             <div class="col-xs-12">
                 <h2 style="text-align: center;"><strong><u style="letter-spacing: 24px;">&nbsp;INVOICE</u></strong></h2>
-                <h2 style="text-align:center"><small>Date: {{date("M d,Y",strtotime(@$getInvoice->created_at))}}</small></h2>
+                <h2 style="text-align:center"><small>Date: {{date("M d,Y",strtotime(@$getInvoice->created_at))}}</small></h2><br>
                 <h4 style="margin-left: 10px;margin-right: 10px;">Invoice Number: {{@$getInvoice->invoice_no}}</h4>
                 <h4 style="margin-left: 10px;margin-right: 10px;"><u>Member Code: {{@$getInvoice->campus->business_school->name}} Campus {{@$getInvoice->campus->location}}</u></h4>
-                <h4 style="margin-left: 10px;margin-right: 10px;border-bottom:1px solid black"> {{@$getInvoice->campus->business_school->name}} Campus {{@$getInvoice->campus->location}} Accreditation registration fee.</h4>
+                <h4 style="margin-left: 10px;margin-right: 10px;border-bottom:1px solid black;padding-bottom:5px;"> {{@$getInvoice->campus->business_school->name}} Campus {{@$getInvoice->campus->location}} Accreditation registration fee.</h4>
             </div>
-            <div class="col-xs-12">
+            <br>
+            
+            <div class="col-xs-12" style="margin-top:10px;">
                 <h4 style="margin: 0px 10px 0px 10px;"><strong class="pull-left">Particular</strong><strong class="pull-right">Amount Rs.</strong></h4><br>
                 <h4 style="margin: 0px 10px 0px 10px;"><span class="pull-left">Registration Fee</span><span class="pull-right">{{number_format(@$getInvoice->amount)}}/-</span></h4>
             </div>
             <div class="col-xs-7"></div>
             <div class="col-xs-5">
-                <h4 style="border-bottom:1px solid black"></h4>
-                <h4 style="border-bottom: 2px solid black;margin-top:20px;margin-left: 10px;margin-right:10px;"><strong style="text-align:left">Total: Rupees. </strong><strong class="pull-right">{{number_format(@$getInvoice->amount)}}/-</strong></h4><br>
+                <h4 style="border-bottom: 2px double black;margin-top:5px;margin-left: 10px;margin-right:10px;border-top:1px solid black;padding-top :10px;"><strong style="text-align:left">Total: Rupees. </strong><strong class="pull-right">{{number_format(@$getInvoice->amount)}}/-</strong></h4><br>
                 <h4 class="pull-right" style="margin-top:70px;margin-left:10px;margin-right:10px;">Asad Khan</h4><br>
                 <h4 class="pull-right" style="margin: 0px 10px 0px 10px;">Accounts-Officer-NBEAC</h4>
             </div>
             <div class="col-xs-12">
-                <p style="font-size: 16px;color:red;margin: 0px 10px 0px 10px;">HEC is Tax Exempted and FTN Number is 9011007-2.:</p><br>
+                <p style="font-size: 16px;margin: 20px 10px 0px 10px;"><span style="color:red !important;background:yellow !important;"> HEC is Tax Exempted and FTN Number is 9011007-2.</span></p><br>
             </div>
             <div class="col-xs-12">
-                <p style="margin: 0px 10px 0px 10px;font-size:16px">Mail us the Cheque bearing the above stated amount in favor of NBEAC-HEC at National Business Education Accreditation Council, Room # 201, HRD Building, Gate # 2, Higher Education Commission, H-8, and Islamabad, Pakistan.</p><br>
+                <p style="margin: 0px 10px 0px 10px;font-size:16px">Mail us the Cheque bearing the above stated amount in favor of <strong>NBEAC-HEC</strong> at National Business Education Accreditation Council, Room # 201, HRD Building, Gate # 2, Higher Education Commission, H-8, and Islamabad, Pakistan.</p><br>
                 <p style="margin: 0px 10px 0px 10px;font-size:16px">CC: Mr. Ahtesham Ali Raja (Program Director-NBEAC)</p>
                 <p style="margin: 0px 10px 0px 10px;font-size:16px">CC: Dr. Farrukh Iqbal (Chairman –NBEAC)</p>
             </div>
@@ -89,7 +105,7 @@
         
 
         <!-- this row will not appear when printing -->
-        <div class="row no-print">
+        <!-- <div class="row no-print">
             <div class="col-xs-12">
                 <a href="invoice-print.html" target="_blank" class="btn btn-default"><i class="fa fa-print"></i> Print</a>
                 {{--          <button type="button" class="btn btn-success pull-right"><i class="fa fa-credit-card"></i> Submit Payment--}}
@@ -98,7 +114,7 @@
                 {{--            <i class="fa fa-download"></i> Generate PDF--}}
                 {{--          </button>--}}
             </div>
-        </div>
+        </div> -->
     </section>
     <!-- /.content -->
     <div class="clearfix"></div>
