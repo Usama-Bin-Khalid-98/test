1a5a5c8c0e5f7bbd7146e1b9ba816a7685b24ff8
Usama Bin Khalid <usama.khalid@arbisoft.com>
2023-05-07 10:34:00 +0500

    Fix: Adding unpaid status to slips

---
+++ database/migrations/2023_04_06_125524_add_unpaid_to_status_in_slips.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+class AddUnpaidToStatusInSlips extends Migration
+
+
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('slips', function (Blueprint $table) {
+            //
+        });
+        DB::statement("ALTER TABLE slips MODIFY COLUMN status ENUM('active', 'inactive','pending', 'paid','approved','unpaid') NOT NULL;");
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('slips', function (Blueprint $table) {
+            //
+        });
+    }
+}
